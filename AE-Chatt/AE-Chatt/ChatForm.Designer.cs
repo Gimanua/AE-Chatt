20d7d5a382d884cc3752812493ea8d38813d9953
Adrian Klasson <gimanua.dota2@gmail.com>
2018-11-22 11:52:34 +0100

    Added tabs and multiple textboxes

--- AE-Chatt/AE-Chatt/ChatForm.Designer.cs
+++ AE-Chatt/AE-Chatt/ChatForm.Designer.cs
@@ -28,32 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Göran");
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Pelle");
-            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.ListViewItem listViewItem11 = new System.Windows.Forms.ListViewItem("Göran");
+            System.Windows.Forms.ListViewItem listViewItem12 = new System.Windows.Forms.ListViewItem("Pelle");
             this.listViewOthers = new System.Windows.Forms.ListView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabControlConversations = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabControlConversations.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // textBoxMessage
-            // 
-            this.textBoxMessage.Location = new System.Drawing.Point(139, 364);
-            this.textBoxMessage.Multiline = true;
-            this.textBoxMessage.Name = "textBoxMessage";
-            this.textBoxMessage.Size = new System.Drawing.Size(649, 74);
-            this.textBoxMessage.TabIndex = 0;
             // 
             // listViewOthers
             // 
             this.listViewOthers.CheckBoxes = true;
-            listViewItem1.StateImageIndex = 0;
-            listViewItem2.StateImageIndex = 0;
+            listViewItem11.StateImageIndex = 0;
+            listViewItem12.StateImageIndex = 0;
             this.listViewOthers.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2});
+            listViewItem11,
+            listViewItem12});
             this.listViewOthers.Location = new System.Drawing.Point(12, 58);
             this.listViewOthers.MultiSelect = false;
             this.listViewOthers.Name = "listViewOthers";
@@ -61,9 +50,7 @@
             this.listViewOthers.TabIndex = 1;
             this.listViewOthers.UseCompatibleStateImageBehavior = false;
             this.listViewOthers.View = System.Windows.Forms.View.List;
-            this.listViewOthers.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
             this.listViewOthers.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.listView1_ItemChecked_1);
-            this.listViewOthers.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listViewOthers_ItemSelectionChanged);
             // 
             // textBox1
             // 
@@ -78,22 +65,13 @@
             // 
             // tabControlConversations
             // 
-            this.tabControlConversations.Controls.Add(this.tabPage1);
             this.tabControlConversations.Location = new System.Drawing.Point(139, 12);
             this.tabControlConversations.Name = "tabControlConversations";
             this.tabControlConversations.SelectedIndex = 0;
             this.tabControlConversations.Size = new System.Drawing.Size(649, 346);
             this.tabControlConversations.TabIndex = 4;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(641, 320);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Conversation";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabControlConversations.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabControlConversations_Selecting);
+            this.tabControlConversations.Selected += new System.Windows.Forms.TabControlEventHandler(this.tabControlConversations_Selected);
             // 
             // ChatForm
             // 
@@ -103,21 +81,16 @@
             this.Controls.Add(this.tabControlConversations);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listViewOthers);
-            this.Controls.Add(this.textBoxMessage);
             this.Name = "ChatForm";
             this.Text = "ChatForm";
-            this.tabControlConversations.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBoxMessage;
         private System.Windows.Forms.ListView listViewOthers;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TabControl tabControlConversations;
-        private System.Windows.Forms.TabPage tabPage1;
     }
 }
--- AE-Chatt/AE-Chatt/ChatForm.cs
+++ AE-Chatt/AE-Chatt/ChatForm.cs
@@ -12,28 +12,40 @@ namespace AE_Chatt
 {
     public partial class ChatForm : Form
     {
+        private List<TextBox> textBoxes = new List<TextBox>();
+
         public ChatForm()
         {
+            TextBox textBoxStandard = new TextBox();
+            AdjustTextBox(textBoxStandard);
+            textBoxStandard.ReadOnly = true;
+            textBoxStandard.Name = "standardIndex";
+            textBoxStandard.Text = "Här skriver du meddelanden";
+            textBoxes.Add(textBoxStandard);
+            Controls.Add(textBoxStandard);
             InitializeComponent();
         }
-
-        private void listView1_ItemActivate(object sender, EventArgs e) { }
+        
 
         private void listView1_ItemChecked_1(object sender, ItemCheckedEventArgs e)
         {
-
-        }
-
-        private void listViewOthers_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)//Fel metod
-        {
-            if (e.IsSelected)
+            if (e.Item.Checked)
             {
-                tabControlConversations.TabPages.Add(e.Item.Text);
+                TabPage tabPage = new TabPage(e.Item.Text);
+                tabPage.Name = e.Item.Text;
+                tabControlConversations.TabPages.Add(tabPage);
                 tabControlConversations.SelectedTab = tabControlConversations.TabPages[tabControlConversations.TabCount - 1];
+                TextBox textBox = new TextBox();
+                textBox.Name = e.Item.Text;
+                AdjustTextBox(textBox);
+                textBoxes.Add(textBox);
+                Controls.Add(textBox);
+                textBox.BringToFront();
             }
             else
             {
-                for(int i = 0; i < tabControlConversations.TabPages.Count; i++)
+                //Ta bort taben
+                for (int i = 0; i < tabControlConversations.TabPages.Count; i++)
                 {
                     if (tabControlConversations.TabPages[i].Text == e.Item.Text)
                     {
@@ -41,6 +53,50 @@ namespace AE_Chatt
                         break;
                     }
                 }
+                //Ta bort textboxen
+                for(int i = 0; i < Controls.Count; i++)
+                {
+                    if(Controls[i].Name == e.Item.Text)
+                    {
+                        Controls.RemoveAt(i);
+                        break;
+                    }
+                }
+                for(int i = 0; i < textBoxes.Count; i++)
+                {
+                    if(textBoxes[i].Name == e.Item.Text)
+                    {
+                        textBoxes.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+        }
+        
+
+        private void AdjustTextBox(TextBox textBox)
+        {
+            textBox.Location = new Point(139, 364);
+            textBox.Multiline = true;
+            textBox.Size = new Size(649, 74);
+            textBox.TabStop = false;
+            textBox.MaxLength = 800;
+        }
+
+        private void tabControlConversations_Selected(object sender, TabControlEventArgs e)
+        {
+            
+        }
+
+        private void tabControlConversations_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            if (e.TabPage == null)
+                return;
+
+            foreach (TextBox textBox in textBoxes)
+            {
+                if (textBox.Name == e.TabPage.Name)
+                    textBox.BringToFront();
             }
         }
     }
